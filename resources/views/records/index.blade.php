a38e4a8c1e6ec77e82d2703c7e1add78218b75e0
lucastoepoel <lucastoepoel@gmail.com>
2025-09-24 14:20:21 +0200

    index update

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -5,16 +5,27 @@
     <title>Test Page</title>
 </head>
 <body>
-    <h1>Welcome to the Test Page</h1>
-    @foreach($records as $record)
-        <div>
-            <strong>ID:</strong> {{ $record->id }}<br>
-            <strong>Title:</strong> {{ $record->title }}<br>
-            <strong>Description:</strong> {{ $record->artist }}<br>
-           
-        </div>
-        <hr>
-    @endforeach
-    <p>This is a basic HTML page.</p>
+<h1>Welcome to the Test Page</h1>
+@foreach($records as $record)
+    <div>
+        <strong>ID:</strong> {{ $record->id }}<br>
+        <strong>Title:</strong> {{ $record->title }}<br>
+
+        <strong>Album:</strong> {{ $record->album->title }}<br>
+
+        <strong>Publisher:</strong> {{ $record->publisher->name }}<br>
+
+        <strong>Artists:</strong>
+        @foreach($record->artists as $artist)
+            {{ $artist->name }} ({{ $artist->pivot->role }})@if(!$loop->last), @endif
+        @endforeach
+        <br>
+        <strong>Genres:</strong>
+        @foreach($record->genres as $genre)
+
+    </div>
+    <hr>
+@endforeach
+<p>This is a basic HTML page.</p>
 </body>
 </html>
