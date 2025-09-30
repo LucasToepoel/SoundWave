6e97645d2059941c2f4021d0157d12d01f25d10e
lucastoepoel <lucastoepoel@gmail.com>
2025-09-30 09:29:48 +0200

    mediacontroller update

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\Album;
 use App\Models\Artist;
 use App\Models\Publisher;
-use Illuminate\Database\Eloquent\Model;zx
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
